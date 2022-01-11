6cf603911b14d5affe0700cf3c0ee1fe16bf96da
alaeddine <alaeddinazri@gmail.com>
2022-01-11 22:11:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const hoursElement = document.getElementById("hour_hand");
+const minutesElement = document.getElementById("minute_hand");
+const secondsElement = document.getElementById("second_hand");
+
+function animate() {
+  const date = new Date();
+
+  const hour = date.getHours() + date.getMinutes() / 60;
+  const minute = date.getMinutes() + date.getSeconds() / 60;
+  const second = date.getSeconds() + date.getMilliseconds() / 1000;
+
+  hoursElement.setAttribute("transform", `rotate(${(360 / 12) * hour})`);
+  minutesElement.setAttribute("transform", `rotate(${(360 / 60) * minute})`);
+  secondsElement.setAttribute("transform", `rotate(${(360 / 60) * second})`);
+
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
